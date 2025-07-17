590570f302702f1d9a310714cb17f280e935fa1a
y-sugawara0508 <sugawara.yuto@marble-corp.co.jp>
2025-07-17 16:52:33 +0900

    init

---
+++ src/main/java/com/example/TodoManager/TodoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.TodoManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/TodoManager/controller/IndexController.java
@@ -0,0 +1,8 @@
+package com.example.TodoManager.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+	
+}
