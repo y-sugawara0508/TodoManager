82cf974b45105e2046b85eb1bb18308a8074a91f
takatera koki <takatera@koki>
2025-08-01 11:58:55 +0900

    add:LoginFormにログインチェックについて追加

--- src/main/java/com/example/TodoManager/form/LoginForm.java
+++ src/main/java/com/example/TodoManager/form/LoginForm.java
@@ -1,10 +1,20 @@
 package com.example.TodoManager.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class LoginForm {
 	
-		
+		/** メールアドレス*/
+		@NotNull
+		@Size(max=200)
 		private String address;
 	 
+		/**パスワード*/
+		@NotBlank
+		@NotNull
+		@Size(max = 16)
 		private String personalPass;
 
 		public String getAddress() {
