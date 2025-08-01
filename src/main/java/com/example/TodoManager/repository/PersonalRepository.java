a7440ab154bd935f91e51ae8c540b6055de5be3d
NiinoYusuke <niino.yusuke@marble-corp.co.jp>
2025-08-01 11:02:06 +0900

    add, ログイン機能

--- src/main/java/com/example/TodoManager/repository/PersonalRepository.java
+++ src/main/java/com/example/TodoManager/repository/PersonalRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.TodoManager.entity.Personal;
 
 public interface PersonalRepository extends JpaRepository<Personal,Integer> {
+	Personal findByAddressAndPersonalPass(String address, String personalPass);
 
 }
