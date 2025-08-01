e9cec72fe7aad711743758250ba2e36cc6d7f0c2
NiinoYusuke <niino.yusuke@marble-corp.co.jp>
2025-08-01 11:20:30 +0900

    fix: 競合の修正

---
+++ src/main/java/com/example/TodoManager/controller/AuthController.java
@@ -0,0 +1,75 @@
+package com.example.TodoManager.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.TodoManager.entity.Personal;
+import com.example.TodoManager.form.LoginForm;
+import com.example.TodoManager.repository.ImportanceRepository;
+import com.example.TodoManager.repository.PersonalRepository;
+import com.example.TodoManager.repository.TaskRepository;
+
+import ch.qos.logback.core.model.Model;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class AuthController {
+
+	@Autowired
+	PersonalRepository pelrepo;
+
+	@Autowired
+	TaskRepository tasrepo;
+
+	@Autowired
+	ImportanceRepository imprepo;
+
+	@Autowired
+	HttpSession session;
+
+	@GetMapping("/")
+	public String showLoginForm(@ModelAttribute LoginForm loginForm) {
+
+		session.invalidate();
+
+		return "login";
+	}
+
+	@PostMapping("/login")
+	public String login(LoginForm loginForm,Model model) {
+
+		// ログインパスワードとかを受け取る
+		String address = loginForm.getAddress();
+
+		// 入力されたパスワードを取得
+		String personalPass = loginForm.getPersonalPass();
+		
+		//メールアドレスとパスワードでDBを検索し、一致する社員情報を取得
+		Personal personal = pelrepo.findByAddressAndPersonalPass(address, personalPass);
+
+		//該当する社員情報が存在する場合(ログイン成功)		
+		if (personal != null) {			
+			return "redirect:/menu";
+
+		} else {
+
+			//個人情報が見つからない場合、エラーメッセージを表示してログイン画面に戻る
+			//model.addAttribute("errMessage", "メールアドレスまたはパスワードが間違っています");
+
+			return "login";
+
+		}
+	}
+
+	@GetMapping("/menu")
+	public String showMenu(Model model) {
+
+		session.invalidate();
+
+		return "menu";
+	}
+
+}
---
+++ src/main/java/com/example/TodoManager/form/LoginForm.java
@@ -0,0 +1,30 @@
+package com.example.TodoManager.form;
+
+public class LoginForm {
+	
+		
+		private String address;
+	 
+		private String personalPass;
+
+		public String getAddress() {
+			return address;
+		}
+
+		public void setAddress(String address) {
+			this.address = address;
+		}
+
+		public String getPersonalPass() {
+			return personalPass;
+		}
+
+		public void setPersonalPass(String personalPass) {
+			this.personalPass = personalPass;
+		}
+
+		
+		
+		
+
+}
