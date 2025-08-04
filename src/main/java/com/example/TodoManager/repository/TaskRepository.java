94f440d04fdb93ef57e2944d002d6944f1dea23c
okamurakichinosuke <okamura.kichinosuke@marble-corp.co.jp>
2025-08-04 16:06:47 +0900

    taskREP

--- src/main/java/com/example/TodoManager/repository/TaskRepository.java
+++ src/main/java/com/example/TodoManager/repository/TaskRepository.java
@@ -1,10 +1,35 @@
 package com.example.TodoManager.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.TodoManager.entity.Importance;
 import com.example.TodoManager.entity.Task;
 
 public interface TaskRepository extends JpaRepository<Task,Integer> {
 	
-
+	@Query ("SELECT t FROM Task t ORDER BY t.deadlineDate = :deadlineDate DESC")
+	List<Task>findbyDeadlineDateQueryDesc(@Param("deadlineDate")Date deadlineDate);
+	
+	
+	@Query ("SELECT t FROM Task t ORDER BY t.deadlineDate = :deadlineDate ASC")
+	List<Task>findbyDeadlineDateQueryAsc(@Param("deadlineDate")Date deadlineDate);
+	 
+	@Query ("SELECT t FROM Task t ORDER BY t.importanceId = :importanceId DESC")
+	List<Task>findbyImportanceIdQueryDesc(@Param("importanceId")Importance importanceId);
+	 
+	@Query ("SELECT t FROM Task t ORDER BY t.importanceId = :importanceId ASC")
+	List<Task>findbyImportanceIdQueryAsc(@Param("importanceId")Importance importanceId);
+	 
+	@Query ("SELECT t FROM Task t ORDER BY t.completeFlag = :completeFlag DESC")
+	List<Task>findbyCompleteFlagQueryDesc(@Param("completeFlag")boolean completeFlag);
+	 
+	@Query ("SELECT t FROM Task t ORDER BY t.completeFlag = :completeFlag ASC")
+	List<Task>findbyCompleteFlagQueryAsc(@Param("completeFlag")boolean completeFlag);
+	
 }
+
