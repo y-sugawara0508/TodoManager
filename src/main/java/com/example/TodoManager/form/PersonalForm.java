2d602e0acd56564f8a0db2cb9cd80a2588a1af84
NiinoYusuke <niino.yusuke@marble-corp.co.jp>
2025-08-04 11:02:55 +0900

    add: ユーザー登録できたたぶん

---
+++ src/main/java/com/example/TodoManager/form/PersonalForm.java
@@ -0,0 +1,63 @@
+package com.example.TodoManager.form;
+
+import java.util.Date;
+
+public class PersonalForm {
+
+	/** ID*/
+	private Integer personalId;
+	
+	/** 名前*/
+	private String personalName;
+	
+	/** メールアドレス*/
+	private String address;
+ 
+	/**パスワード*/
+	private String personalPass;
+	
+	/** 生年月日*/
+	private Date birthday;
+
+	public Integer getPersonalId() {
+		return personalId;
+	}
+
+	public void setPersonalId(Integer personalId) {
+		this.personalId = personalId;
+	}
+
+	public String getPersonalName() {
+		return personalName;
+	}
+
+	public void setPersonalName(String personalName) {
+		this.personalName = personalName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPersonalPass() {
+		return personalPass;
+	}
+
+	public void setPersonalPass(String personalPass) {
+		this.personalPass = personalPass;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	
+	
+}
