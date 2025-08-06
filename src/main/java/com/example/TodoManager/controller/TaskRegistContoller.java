53b5502c3ed716cec5fbbcf78a940bbd5b555c59
b25a0146 <b25a0146@tcs-one.com>
2025-08-06 09:45:48 +0900

    8月5日　新規タスク登録機能完成

--- src/main/java/com/example/TodoManager/controller/TaskRegistContoller.java
+++ src/main/java/com/example/TodoManager/controller/TaskRegistContoller.java
@@ -1,11 +1,13 @@
 package com.example.TodoManager.controller;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +17,13 @@ import com.example.TodoManager.form.TaskForm;
 import com.example.TodoManager.repository.TaskRepository;
 
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 
 @Controller
 public class TaskRegistContoller {
-	
+
 	@Autowired
 	TaskRepository repository;
-	
+
 	/**
 	 * 新規タスク登録入力画面遷移の為のメソッド
 	 * @return 新規タスク登録入力画面のURL
@@ -31,20 +32,22 @@ public class TaskRegistContoller {
 	public String showTaskRegistForm(@ModelAttribute TaskForm form) {
 		return "task/regist/input";
 	}
-	
-	
+
 	/**
 	 * 新規タスク登録確認画面の為のメソッド
 	 * @return 新規タスク登録確認画面のURL
-	 */
+	 
 	@PostMapping("/task/regist/check")
 	public String taskRegistCheck(@Valid @ModelAttribute TaskForm form, BindingResult result, Model model, HttpSession session) {
 		if(result.hasErrors()) {
-			System.out.println("ああ");
+			
+			System.out.println(result.getErrorCount());
+			
+			System.out.println("no");
 			System.out.println(result.hasErrors());
 			return "task/regist/input";
 		}else {
-			
+			System.out.println("ok");
 			Integer personalId = (Integer) session.getAttribute("personalId");
 			form.setPersonalId(personalId);
 			//TaskFormをリクエストスコープに保存
@@ -53,17 +56,44 @@ public class TaskRegistContoller {
 		}
 		
 	}
-	
+	*/
+
+	/**
+	 * 新規タスク登録確認画面の為のメソッドの入力チェックなし
+	 * @param form
+	 * @param model
+	 * @param session
+	 * @return
+	 */
+	@PostMapping("/task/regist/check")
+	public String taskRegistCheck(@ModelAttribute TaskForm form, Model model, HttpSession session) {
+		session.setAttribute("date", form.getDate());
+		session.setAttribute("deadlinetime", form.getDeadlineTime());
+
+		LocalDate date = (LocalDate) session.getAttribute("date");
+		LocalTime deadlinetime = (LocalTime) session.getAttribute("deadlinetime");
+
+		System.out.println(LocalDateTime.of(date, deadlinetime));
+		LocalDateTime deadlineDate = LocalDateTime.of(date, deadlinetime);
+		form.setDeadlineDate(deadlineDate);
+
+		Integer personalId = (Integer) session.getAttribute("personalId");
+		form.setPersonalId(personalId);
+		model.addAttribute("taskForm", form);
+		return "task/regist/check";
+	}
+
 	/**
 	 * 新規タスク登録完了画面の為のメソッド
 	 * @return　新規タスク登録完了画面URL
 	 */
 	@PostMapping("/task/regist/complete")
 	public String taskRegistComplete(TaskForm form) {
+		System.out.println(form.getDeadlineDate());
 		Task task = new Task();
-		task.setDeleteFlag(true);
-		task.setCompleteFlag(true);
-		task.setArchiveFlag(true);
+		task.setDeleteFlag(false);
+		task.setCompleteFlag(false);
+		task.setArchiveFlag(false);
 		BeanUtils.copyProperties(form, task, "taskId");
 		repository.save(task);
 		return "task/regist/complete";
